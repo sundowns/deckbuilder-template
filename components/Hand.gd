670dd9f1ea8d32a7d2b14b371f9257b4379c344d
sundowns <tom@smallridge.com.au>
2024-06-27 21:17:44 +0800

    update todos

--- components/Hand.gd
+++ components/Hand.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Hand
 
+# TODO: this class will manage keeping its children in order and in position similar to balatro (cause its cool :])
+
 func _on_card_gained(card: Card) -> void:
 	print("added a card to hand: ", card)
 	var playable: PlayableCard = Cards.create_playable_card(card)
